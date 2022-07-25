cf4f977a4a6c4e5630e4ec3a2ac7fb1b456f53f6
Wynter Alford <77499203+wynter-alford@users.noreply.github.com>
2022-07-25 14:24:26 -0400

    Short-term fix to dipolar coupling distribution
    As a short-term fix until a better method is refined, dipolar couplings d_ij are drawn uniformly from 0-2 instead of normally about 0.  This should eliminate the problem of single large couplings skewing results heavily.
    `Standard_Dipole_Hamiltonians(4)` has been renamed to `Normal_Dipole_Hamiltonians(4).mat` (containing the same set of matrices).  The default set of couplings are now `Uniform02_Dipole_Hamiltonians(4).mat`.

--- code/initHamiltonians.m
+++ code/initHamiltonians.m
@@ -3,13 +3,13 @@
 % 
 % Adapted from pre-existing code into its own script for ease of use
 
-if N==4&&couplingsCount<=4
-    load('Standard_Dipole_Hamiltonians(4).mat','Hdips');
+if N==400&&couplingsCount<=4
+    load('Uniform02_Dipole_Hamiltonians(4).mat','Hdips');
 elseif ~exist('Hdips','var')
      Hdips = cell(couplingsCount,1);
      % Generate [couplingsCount] dipole Hamiltonians, each with a different coupling matrix
      for j=1:couplingsCount
-         dip = abs(randn(N));
+         dip = random('Uniform',0.1,2,N);
          dip = triu(dip,1) + triu(dip,1)';
          Hdips{j} = getHdip(N, dim, x, y, z, dip);
      end
